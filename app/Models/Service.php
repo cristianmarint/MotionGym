2d3a228077b3dbcd5e3d3613f3e500a7adeae4a3
cristianmarint <cristianmarint@gmail.com>
2019-08-01 16:06:53 -0500

    Minor change on Service model

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -15,6 +15,6 @@ class Service extends Model
         'name',
         'photo',
         'description',
-        'precio'
+        'price'
     ];
 }
--- database/migrations/2019_07_02_030905_create_service_table.php
+++ database/migrations/2019_07_02_030905_create_service_table.php
@@ -3,7 +3,7 @@
  * @ Author: @CristianMarinT
  * @ Create Time: 2019-07-01 22:03:05
  * @ Modified by: @CristianMarinT
- * @ Modified time: 2019-07-24 21:19:59
+ * @ Modified time: 2019-08-01 13:22:04
  * @ Description:
  */
 
@@ -26,7 +26,7 @@ class CreateServiceTable extends Migration
             $table->string('name',250);
             $table->string('photo', 500)->default(null)->nullable(true);          
             $table->text('description');
-            $table->decimal('precio', 8, 2);
+            $table->decimal('price', 8, 2);
 
             $table->softDeletes();
             $table->timestamps();
